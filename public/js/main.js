268abc52a6ffd8a04e23bc206cea823e0f926512
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-09-09 18:44:45 +0600

    make yandex map with api

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,71 @@
 let formRoutes = document.forms.routes;
 
+let createRouteButton = formRoutes.createRoute
+
+let YaMap = document.querySelector('#YaMap')
+
+let addMoreButton = formRoutes.addMore
+
+let inputsContainer = document.querySelector('.inputs_container')
+
+createRouteButton.disabled = true
+
 formRoutes.addEventListener('submit', (event) => {
   event.preventDefault()
-  let centerPos = formRoutes.center.value;
+})
 
-  ymaps.ready(init(centerPos));
+let apiLoaded = async () => {
+  await ymaps.ready()
+  createRouteButton.disabled = false
+}
+
+apiLoaded()
+
+addMoreButton.addEventListener('click', () => {
+  let newInput = `<input placeholder="Add address" name="address" type="text">`
+  let inputs = document.getElementsByTagName('input')
+
+  if (inputs.length < 4) {
+    inputsContainer.insertAdjacentHTML('beforeend', newInput)
+  } else {
+    addMoreButton.disabled = true
+  }
+})
+
+createRouteButton.addEventListener('click', async (event) => {
+  if (YaMap.children.length) YaMap.children[0].remove()
+  let data = []
+  let inputs = document.getElementsByTagName('input')
+  for (let i = 0; i < inputs.length; i++) {
+    data.push(inputs[i].value)
+  }
+  console.log(data)
+  init(data)
 });
 
-function init(centerPos) {
-  var myMap;
-  ymaps.geocode(centerPos).then(function (res) {
-    myMap = new ymaps.Map('YaMap', {
-      center: res.geoObjects.get(0).geometry.getCoordinates(),
-      zoom: 16
+function init(data) {
+    /**
+     * Создаем мультимаршрут.
+     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+     */
+
+    multiRoute = new ymaps.multiRouter.MultiRoute({
+      referencePoints: data,
+      params: {
+        //Тип маршрутизации - пешеходная маршрутизация.
+        routingMode: 'pedestrian'
+      }
+    }, {
+      // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+      boundsAutoApply: true
     });
-  });
+
+    let myMap = new ymaps.Map('YaMap', {
+      center: [55.739625, 37.54120],
+      zoom: 12,
+    });
+
+    // Добавляем мультимаршрут на карту.
+    myMap.geoObjects.add(multiRoute);
 }
+
